6979365bfbcaf7908dcf2b42a50c96262ba1c2d1
alchern2412 <mynameislyosha2412@yandex.com>
2020-04-16 14:51:46 +0300

    add api/users, api/auth

--- models/User.js
+++ models/User.js
@@ -9,11 +9,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
-    }, 
+    },
     password: {
         type: String,
         required: true
-    }, 
+    },
     avatar: {
         type: String
     },
