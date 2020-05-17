4447c54b86c401aa5c076057f7542bda60191f3a
alchern2412 <mynameislyosha2412@yandex.com>
2020-05-17 17:17:50 +0300

    combine reducers

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password,} = formData
+
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+
+    const onSubmit = async e => {
+        e.preventDefault()
+        console.log('SUCCESS')
+
+    }
+
+    return (
+        <>
+            <h1 className="large text-primary">
+                Sign In
+            </h1>
+            <p className="lead"><i className="fas fa-user"></i>
+                Sign Into Your Account
+            </p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        name='email'
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        minLength="6"
+                        onChange={e => onChange(e)}
+                        name='password'
+                        value={password}
+                    />
+                </div>
+                <input type="submit" value="Login" className="btn btn-primary"/>
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </>
+    )
+}
+
+export default Login
