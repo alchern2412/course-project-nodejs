ddcbac88b0b5d028678e0686a0238842c69e9cec
alchern2412 <mynameislyosha2412@yandex.com>
2020-05-27 00:31:54 +0300

    Fix class => className, PropTypes

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -20,11 +20,11 @@ const CommentItem = ({
     deleteComment
 }) => {
     return (
-        <div class="post bg-white p-1 my-1">
+        <div className="post bg-white p-1 my-1">
             <div>
                 <Link to={`/profile/${user}`}>
                     <img
-                        class="round-img"
+                        className="round-img"
                         src={avatar}
                         alt=""
                     />
@@ -32,10 +32,10 @@ const CommentItem = ({
                 </Link>
             </div>
             <div>
-                <p class="my-1">
+                <p className="my-1">
                     {text}
                 </p>
-                <p class="post-date">Posted on <Moment format="YYYY/MM/DD">{date}</Moment></p>
+                <p className="post-date">Posted on <Moment format="YYYY/MM/DD">{date}</Moment></p>
                 {
                     !auth.loading && user === auth.user._id && (
                         <button
@@ -53,7 +53,7 @@ const CommentItem = ({
 }
 
 CommentItem.propTypes = {
-    postId: PropTypes.number.isRequired,
+    postId: PropTypes.string.isRequired,
     comment: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
     deleteComment: PropTypes.func.isRequired
